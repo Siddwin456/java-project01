53b6e9e720a89ee40096daccd524ba7fcfe62199
Siddwin456 <siddhivinayakamech.rymec@gmail.com>
2023-11-02 14:33:27 +0530

    first commit

---
+++ src/main/java/myPrograms/FirstProgram.java
@@ -0,0 +1,23 @@
+package myPrograms;
+
+public class FirstProgram {
+
+    public static void main(String[] args){
+        int i = 1;
+        int j = 20;  //Variable declaration and initialization
+        int c;   //variable declaration
+        c = i+j; // initializing i+j data to c
+        System.out.println(c);// print statement (output: 21)
+
+        if (c == 22){  // "if" condition
+            System.out.println("yes it is 21");
+        }
+        else {  //  else block
+            for(int k = 1; k <= 5; k++){   // "for" loop
+                System.out.println("my name is siddi vinayaka");
+            }
+        }
+
+
+    }
+}
